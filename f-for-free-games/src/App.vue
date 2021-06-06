b57b2dcc68f0fb48990ade0418bd01e1ddf06f80
frvalenzuela <frvalenzuela@uc.cl>
2021-06-06 13:09:24 -0400

    finish-bigdiv

--- f-for-free-games/src/App.vue
+++ f-for-free-games/src/App.vue
@@ -1,15 +1,23 @@
 <template>
   <h1>Encuentra tu juego ideal!</h1>
   <SearchBar />
+  <BigDiv
+    @toggle-reminder="toggleReminder"
+    :games="games"
+    :selected_game="selected_game"
+  />
 </template>
 
 <script>
 import SearchBar from "./components/SearchBar.vue";
+import BigDiv from "./components/BigDiv.vue";
+
 
 export default {
   name: "App",
   components: {
-    SearchBar
+    SearchBar,
+    BigDiv
   },
   props: {
     options: {
@@ -26,14 +34,24 @@ export default {
   },
   data () {
     return {
-      games: []
+      games: [],
+      selected_game: 0
+    }
+  },
+  methods: {
+    toggleReminder(id){
+      if (this.selected_game === id) {
+        this.selected_game = 0
+      } else {
+        this.selected_game = id
+      }
     }
   },
   async created() {
     const req = await fetch("https://free-to-play-games-database.p.rapidapi.com/api/games", this.options)
       .then(aux => (aux.json()))
       .then(res => this.games = res)
-    console.log(this.games)
+    console.log(this.games, this.games.keys())
   }
 };
 </script>
@@ -43,6 +61,7 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  align-items: center;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
---
+++ f-for-free-games/src/components/GameDiv.vue
@@ -0,0 +1,49 @@
+<template>
+    <img
+        @click="$emit('toggle-reminder', game.id)"
+        :class="[selected_game == game.id ? 'big-img-game' : 'img-game']"
+        v-bind:src="game.thumbnail"
+    >
+    <span>{{ game.title}}</span>
+    <div>
+        <h6>Name: {{ game.title }}</h6>
+    <h6>Description: {{ game.short_description }}</h6>
+
+    <h6>Genre: {{ game.genre }}</h6>
+    <h6>Platform: {{ game.platform }}</h6>
+    <a  v-bind:href="game.game_url">DOWNLOAD NOW!</a>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'GameDiv',
+    props: {
+        game: Object,
+        selected_game: Number,
+    }
+}
+</script>
+
+<style>
+
+.img-game{
+    width: 90%;
+    height: 70%;
+    position: relative;
+    display: inline-block;
+    overflow: hidden;
+    margin: 0;
+}
+
+.big-img-game{
+    width: 50%;
+    height: 50%;
+    position: flex;
+    display: block;
+    overflow: hidden;
+    margin: 0;
+    float: left;
+}
+
+</style>
